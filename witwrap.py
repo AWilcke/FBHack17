78539c19f91f474cd966cff281e9b6b448126c7d
Alex Olson <mootwo14@gmail.com>
2017-03-11 15:27:25 +0000

    attempt to recover if the stock name isn't identified

--- witwrap.py
+++ witwrap.py
@@ -1,4 +1,5 @@
 import os
+import re
 from wit import Wit
 
 sample = {'msg_id': '281e0caf-9784-47c0-8093-423d35e87faa', '_text': 'Let me know when GOOG hits $500', 'entities': {'number': [{'confidence': 1, 'value': 500, 'type': 'value'}], 'stock': [{'confidence': 1, 'value': 'GOOG', 'type': 'value'}], 'currency': [{'confidence': 1, 'value': '$', 'type': 'value'}], 'change': [{'confidence': 1, 'value': 'reaches', 'type': 'value'}]}}
@@ -29,7 +30,14 @@ def parse_message(msg, witstance):
         (dicts,key) in [(response[key],key) for key in response] 
     for dict in dicts
     ]
-    return dict((x, y) for x, y in tupform)
+    fin = dict((x, y) for x, y in tupform)
+    if 'stock' not in fin:
+        stock = re.findall('\$([^ ]{3,10})', fin)
+        if len(stock) != 1:
+            raise KeyError('failed to identify stock name!')
+        else:
+            fin['stock'] = stock[0]
+    return fin
 
 if __name__ == "__main__":
     print(test_pm())
