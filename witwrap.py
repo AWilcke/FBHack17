ca64b6ac1dab5b9701cefde86a85d2d9c29f1630
Alex Olson <mootwo14@gmail.com>
2017-03-11 14:56:34 +0000

    Python wrapper for wit rapper for python

---
+++ witwrap.py
@@ -0,0 +1,35 @@
+import os
+from wit import Wit
+
+sample = {'msg_id': '281e0caf-9784-47c0-8093-423d35e87faa', '_text': 'Let me know when GOOG hits $500', 'entities': {'number': [{'confidence': 1, 'value': 500, 'type': 'value'}], 'stock': [{'confidence': 1, 'value': 'GOOG', 'type': 'value'}], 'currency': [{'confidence': 1, 'value': '$', 'type': 'value'}], 'change': [{'confidence': 1, 'value': 'reaches', 'type': 'value'}]}}
+
+def get_sample():
+    return sample
+
+def configure_wit(actions={}):
+    wit_key = os.environ.get('WIT_KEY')
+    if wit_key is None:
+        raise EnvironmentError("Couldn't retrieve client key")
+    wit_here = Wit(access_token=wit_key, actions=actions)
+    return wit_here
+
+def test_pm():
+    response = sample['entities']
+    tupform = [
+    (key, dict['value']) for 
+        (dicts,key) in [(response[key],key) for key in response] 
+    for dict in dicts
+    ]
+    return dict((x, y) for x, y in tupform)
+
+def parse_message(msg, witstance):
+    response = witstance.message(msg)['entities']
+    tupform = [
+    (key, dict['value']) for 
+        (dicts,key) in [(response[key],key) for key in response] 
+    for dict in dicts
+    ]
+    return dict((x, y) for x, y in tupform)
+
+if __name__ == "__main__":
+    print(test_pm())
