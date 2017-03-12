433092e42afefb3187f511131fce3bd72181bb3a
Alex Olson <mootwo14@gmail.com>
2017-03-12 00:08:07 +0000

    Final

--- witwrap.py
+++ witwrap.py
@@ -89,7 +89,7 @@ def process_dict(responsedict):
                     if len(y) == 2:
                         responsedict['lesser'].append(y[0])
                         responsedict['greater'].append(y[1])
-                        del responsedict[x]
+                        del responsedict[s]
             del responsedict['comparison']
         elif "greater" in responsedict['comparison']:
             for (t, y) in responsedict.items():
@@ -128,9 +128,9 @@ if __name__ == "__main__":
     z = configure_wit()
     while rawin != "exit":
         rawin = raw_input(">>>")
-        try:
-            x = (parse_message(rawin, z))
-            print(x)
+#        try:
+        x = (parse_message(rawin, z))
+        print(x)
 #        except Exception as e:
 #            print(str(e))
 #            pass
